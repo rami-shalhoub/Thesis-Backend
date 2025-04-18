fb3d937434df76ce86adc6114bfb726d85c6def1
rami shalhoub <rami2002.work@gmail.com>
2025-04-18 16:34:43 +0200

    Creating the Backend + scaffolding the database

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Backend.data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ThesisDappDBContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
